aa8e5295be82c86205e38792c8d72cc471f41d0e
Harvey Yerik <yerik05vh@gmail.com>
2022-07-15 12:58:16 -0500

    added link

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -45,11 +45,27 @@ function Content() {
                 className="btnExit"
                 onClick={handleExit}
               />
-              <a className="viewApp">{texts.btnApp}</a>
-              <a className="viewCode">{texts.btnCode}</a>
+              <a
+                target="_blank"
+                href="https://yerikah.github.io/briefcase/"
+                className="viewApp"
+              >
+                {texts.btnApp}
+              </a>
+              <a
+                target="_blank"
+                href="https://github.com/YerikAH/briefcase"
+                className="viewCode"
+              >
+                {texts.btnCode}
+              </a>
             </div>
-            <h1>{texts.title}</h1>
-            <p>{texts.text}</p>
+            <h1>{texts.titleOne}</h1>
+            <p>
+              {texts.textOneFirst}
+              <i className="tech">{texts.reactText}</i>
+              {texts.textOneSecond}
+            </p>
           </div>
           <div className="child-grid">
             <svg
@@ -75,11 +91,28 @@ function Content() {
                 className="btnExit"
                 onClick={handleExit}
               />
-              <a className="viewApp">{texts.btnApp}</a>
-              <a className="viewCode">{texts.btnCode}</a>
+              <a
+                target="_blank"
+                href="https://yerikah.github.io/challenge-front-end/Blogr/public/"
+                className="viewApp"
+              >
+                {texts.btnApp}
+              </a>
+
+              <a
+                target="_blank"
+                href="https://github.com/YerikAH/challenge-front-end/tree/main/Blogr"
+                className="viewCode"
+              >
+                {texts.btnCode}
+              </a>
             </div>
-            <h1>{texts.title}</h1>
-            <p>{texts.text}</p>
+            <h1>{texts.titleTwo}</h1>
+            <p>
+              {texts.textTwoFirst}
+              <i className="tech">{texts.sassText}</i>
+              {texts.textTwoSecond}
+            </p>
           </div>
           <div className="child-grid">
             <svg
@@ -105,11 +138,28 @@ function Content() {
                 className="btnExit"
                 onClick={handleExit}
               />
-              <a className="viewApp">{texts.btnApp}</a>
-              <a className="viewCode">{texts.btnCode}</a>
+              <a
+                target="_blank"
+                href="https://yerikah.github.io/challenge-front-end/rock-paper-scissors/public/"
+                className="viewApp"
+              >
+                {texts.btnApp}
+              </a>
+
+              <a
+                target="_blank"
+                href="https://github.com/YerikAH/challenge-front-end/tree/main/rock-paper-scissors"
+                className="viewCode"
+              >
+                {texts.btnCode}
+              </a>
             </div>
-            <h1>{texts.title}</h1>
-            <p>{texts.text}</p>
+            <h1>{texts.titleThree}</h1>
+            <p>
+              {texts.textThreeFirst}
+              <i className="tech">{texts.jsText}</i>
+              {texts.textThreeSecond}
+            </p>
           </div>
           <div className="child-grid">
             <svg
@@ -135,11 +185,28 @@ function Content() {
                 className="btnExit"
                 onClick={handleExit}
               />
-              <a className="viewApp">{texts.btnApp}</a>
-              <a className="viewCode">{texts.btnCode}</a>
+              <a
+                target="_blank"
+                href="https://yerikah.github.io/easybank-frontend-mentor/"
+                className="viewApp"
+              >
+                {texts.btnApp}
+              </a>
+
+              <a
+                target="_blank"
+                href="https://github.com/YerikAH/easybank-frontend-mentor"
+                className="viewCode"
+              >
+                {texts.btnCode}
+              </a>
             </div>
-            <h1>{texts.title}</h1>
-            <p>{texts.text}</p>
+            <h1>{texts.titleFour}</h1>
+            <p>
+              {texts.textFourFirst}
+              <i className="tech">{texts.styled}</i>
+              {texts.textFourSecond}
+            </p>
           </div>
           <div className="child-grid">
             <svg
@@ -165,11 +232,28 @@ function Content() {
                 className="btnExit"
                 onClick={handleExit}
               />
-              <a className="viewApp">{texts.btnApp}</a>
-              <a className="viewCode">{texts.btnCode}</a>
+              <a
+                target="_blank"
+                href="https://yerikah.github.io/my-task-app/public/#"
+                className="viewApp"
+              >
+                {texts.btnApp}
+              </a>
+
+              <a
+                target="_blank"
+                href="https://github.com/YerikAH/my-task-app"
+                className="viewCode"
+              >
+                {texts.btnCode}
+              </a>
             </div>
-            <h1>{texts.title}</h1>
-            <p>{texts.text}</p>
+            <h1>{texts.titleFive}</h1>
+            <p>
+              {texts.textFiveFirst}
+              <i className="tech">{texts.jsText}</i>
+              {texts.textFiveSecond}
+            </p>
           </div>
           <div className="child-grid">
             <svg
@@ -195,11 +279,28 @@ function Content() {
                 className="btnExit"
                 onClick={handleExit}
               />
-              <a className="viewApp">{texts.btnApp}</a>
-              <a className="viewCode">{texts.btnCode}</a>
+              <a
+                target="_blank"
+                href="https://yerikah.github.io/challenge-front-end/manage-landing-page/"
+                className="viewApp"
+              >
+                {texts.btnApp}
+              </a>
+
+              <a
+                target="_blank"
+                href="https://github.com/YerikAH/challenge-front-end/tree/main/manage-landing-page"
+                className="viewCode"
+              >
+                {texts.btnCode}
+              </a>
             </div>
-            <h1>{texts.title}</h1>
-            <p>{texts.text}</p>
+            <h1>{texts.titleSix}</h1>
+            <p>
+              {texts.textSixFirst}
+              <i className="tech">{texts.jsText}</i>
+              {texts.textSixSecond}
+            </p>
           </div>
           <div className="child-grid">
             <svg
@@ -225,11 +326,28 @@ function Content() {
                 className="btnExit"
                 onClick={handleExit}
               />
-              <a className="viewApp">{texts.btnApp}</a>
-              <a className="viewCode">{texts.btnCode}</a>
+              <a
+                target="_blank"
+                href="https://yerikah.github.io/poke-search/"
+                className="viewApp"
+              >
+                {texts.btnApp}
+              </a>
+
+              <a
+                target="_blank"
+                href="https://github.com/YerikAH/poke-search"
+                className="viewCode"
+              >
+                {texts.btnCode}
+              </a>
             </div>
-            <h1>{texts.title}</h1>
-            <p>{texts.text}</p>
+            <h1>{texts.titleSeven}</h1>
+            <p>
+              {texts.textSevenFirst}
+              <i className="tech">{texts.jsText}</i>
+              {texts.textSevenSecond}
+            </p>
           </div>
           <div className="child-grid">
             <svg
@@ -255,11 +373,75 @@ function Content() {
                 className="btnExit"
                 onClick={handleExit}
               />
-              <a className="viewApp">{texts.btnApp}</a>
-              <a className="viewCode">{texts.btnCode}</a>
+              <a
+                target="_blank"
+                href="https://github.com/YerikAH/app-weather"
+                className="viewApp"
+              >
+                {texts.btnApp}
+              </a>
+
+              <a
+                target="_blank"
+                href="https://yerikah.github.io/app-weather/public/"
+                className="viewCode"
+              >
+                {texts.btnCode}
+              </a>
             </div>
-            <h1>{texts.title}</h1>
-            <p>{texts.text}</p>
+            <h1>{texts.titleEig}</h1>
+            <p>
+              {texts.textEigFirst}
+              <i className="tech">{texts.jsText}</i>
+              {texts.textEigSecond}
+            </p>
+          </div>
+
+          <div className="child-grid">
+            <svg
+              width="22"
+              height="20"
+              viewBox="0 0 22 20"
+              fill="none"
+              className="settings"
+              onClick={handleClick}
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                className="pathColor"
+                d="M0.78125 3.51562H21.0938C21.5252 3.51562 21.875 3.16587 21.875 2.73438V0.78125C21.875 0.349756 21.5252 0 21.0938 0H0.78125C0.349756 0 0 0.349756 0 0.78125V2.73438C0 3.16587 0.349756 3.51562 0.78125 3.51562ZM0.78125 11.3281H21.0938C21.5252 11.3281 21.875 10.9784 21.875 10.5469V8.59375C21.875 8.16226 21.5252 7.8125 21.0938 7.8125H0.78125C0.349756 7.8125 0 8.16226 0 8.59375V10.5469C0 10.9784 0.349756 11.3281 0.78125 11.3281ZM0.78125 19.1406H21.0938C21.5252 19.1406 21.875 18.7909 21.875 18.3594V16.4062C21.875 15.9748 21.5252 15.625 21.0938 15.625H0.78125C0.349756 15.625 0 15.9748 0 16.4062V18.3594C0 18.7909 0.349756 19.1406 0.78125 19.1406Z"
+                fill="#4A4C5B"
+              />
+            </svg>
+
+            <div className="hover">
+              <img
+                src={quit}
+                alt="quit"
+                className="btnExit"
+                onClick={handleExit}
+              />
+              <a
+                target="_blank"
+                href="https://yerikah.github.io/challenge-front-end/advice-generator-app/"
+                className="viewApp"
+              >
+                {texts.btnApp}
+              </a>
+
+              <a
+                target="_blank"
+                href="https://github.com/YerikAH/challenge-front-end/tree/main/advice-generator-app"
+                className="viewCode"
+              >
+                {texts.btnCode}
+              </a>
+            </div>
+            <h1>{texts.titleNine}</h1>
+            <p>
+              {texts.textNineFirst}
+              <i className="tech">{texts.jsText}</i>.
+            </p>
           </div>
           <div className="child-grid">
             <svg
@@ -285,11 +467,27 @@ function Content() {
                 className="btnExit"
                 onClick={handleExit}
               />
-              <a className="viewApp">{texts.btnApp}</a>
-              <a className="viewCode">{texts.btnCode}</a>
+              <a
+                target="_blank"
+                href="https://yerikah.github.io/challenge-front-end/interactive-rating/"
+                className="viewApp"
+              >
+                {texts.btnApp}
+              </a>
+
+              <a
+                target="_blank"
+                href="https://github.com/YerikAH/challenge-front-end/tree/main/interactive-rating"
+                className="viewCode"
+              >
+                {texts.btnCode}
+              </a>
             </div>
-            <h1>{texts.title}</h1>
-            <p>{texts.text}</p>
+            <h1>{texts.titleTen}</h1>
+            <p>
+              {texts.textTenFirst}
+              <i className="tech">{texts.jsText}</i>.
+            </p>
           </div>
           <div className="child-grid">
             <svg
@@ -315,11 +513,27 @@ function Content() {
                 className="btnExit"
                 onClick={handleExit}
               />
-              <a className="viewApp">{texts.btnApp}</a>
-              <a className="viewCode">{texts.btnCode}</a>
+              <a
+                target="_blank"
+                href="https://yerikah.github.io/challenge-front-end/expenses/"
+                className="viewApp"
+              >
+                {texts.btnApp}
+              </a>
+
+              <a
+                target="_blank"
+                href="https://github.com/YerikAH/challenge-front-end/tree/main/expenses"
+                className="viewCode"
+              >
+                {texts.btnCode}
+              </a>
             </div>
-            <h1>{texts.title}</h1>
-            <p>{texts.text}</p>
+            <h1>{texts.titleElev}</h1>
+            <p>
+              {texts.textElevFirst}
+              <i className="tech">{texts.jsText}</i>.
+            </p>
           </div>
           <div className="child-grid">
             <svg
@@ -345,11 +559,27 @@ function Content() {
                 className="btnExit"
                 onClick={handleExit}
               />
-              <a className="viewApp">{texts.btnApp}</a>
-              <a className="viewCode">{texts.btnCode}</a>
+              <a
+                target="_blank"
+                href="https://yerikah.github.io/challenge-front-end/modal/"
+                className="viewApp"
+              >
+                {texts.btnApp}
+              </a>
+
+              <a
+                target="_blank"
+                href="https://github.com/YerikAH/challenge-front-end/tree/main/modal"
+                className="viewCode"
+              >
+                {texts.btnCode}
+              </a>
             </div>
-            <h1>{texts.title}</h1>
-            <p>{texts.text}</p>
+            <h1>{texts.titleTwl}</h1>
+            <p>
+              {texts.textTwlFirst}
+              <i className="tech">{texts.DuoFron}</i>.
+            </p>
           </div>
         </div>
       </div>
--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -7,15 +7,134 @@ const translations = {
     lan: "ES",
     btnCode: "ver codigo",
     btnApp: "ver app",
-    title: "app-tareas-lista",
-    text: "Un juego que fue desarrollado con HTML, CSS y JavaScript, un juego muy conocido se trata de elegir entre papel, tijera o piedra, el programa tambien eligira uno.",
+    titleOne: "mi-portafolio",
+    /*tech*/
+    reactText: "React y CSS",
+    sassText: "HTML, SASS y JavaScript",
+    jsText: "HTML, CSS y JavaScript",
+    styled: "React y styled-component",
+    DuoFron: "HTML y CSS",
+    /*first project*/
+    textOneFirst: "Una aplicación que fue desarrollada en ",
+    textOneSecond: " tiene un dark mode, y algunas asombrosas opciones más.",
+    /*second project*/
+    titleTwo: "app-blogr",
+    textTwoFirst:
+      "Una pequeña aplicación tipo landing page que fue desarrollada con ",
+    textTwoSecond: " el diseño pertenece a Front end Mentor.",
+
+    /*thri project*/
+    titleThree: "piedra-papel-tijera",
+    textThreeFirst: "Un juego que fue desarrollado en   ",
+    textThreeSecond:
+      ", el juego trata de elegir entre 3 opciones piedra, papel o tijera.",
+
+    /*four project*/
+    titleFour: "easybank",
+    textFourFirst: "Una aplicación que fue desarrollado con ",
+    textFourSecond: ". El diseño es de Front end Mentor.",
+
+    /*five project*/
+    titleFive: "app-tareas-lista",
+    textFiveFirst:
+      "Una aplicación de tareas que contiene también un pomodoro fue desarrollado con  ",
+    textFiveSecond: ". Tiene muchas opciones más.",
+
+    /*six project*/
+    titleSix: "administrador-lading-page",
+    textSixFirst: "Una aplicación que fue desarrollado con  ",
+    textSixSecond:
+      ". En este proyecto aprendí a usar “max-width”, el diseño es de Front end Mentor.",
+    /*seven project*/
+    titleSeven: "buscar-pokemon",
+    textSevenFirst:
+      "Una aplicación simple, Que permite buscar algunos pokemon. Fue desarrollado con   ",
+    textSevenSecond: ". La API que fue consumida es de POKEAPI.",
+    /*eig project*/
+    titleEig: "app-tiempo",
+    textEigFirst: "Una aplicación que fue desarrollado con  ",
+    textEigSecond: ". Permite ver el tiempo consumido una API.",
+    /*nine project*/
+    titleNine: "generador-de-consejos",
+    textNineFirst:
+      "Un modal que te da grandes consejos de un modo aleatorio, Esta consume un api además que se desarrolló con ",
+    /*ten project*/
+    titleTen: "calificación-interactiva ",
+    textTenFirst:
+      "Un modal que simula la interacción de un usuario al ponerle una valoración a un producto. Fue desarrollado con ",
+    /*elev project*/
+    titleElev: "gráfico-gastos",
+    textElevFirst:
+      "Una aplicación que permite visualizar un gráfico de forma interactiva en Mobile y en desktop. Fue desarrollado en ",
+    /*Twl project*/
+    titleTwl: "modal-orden-pedido",
+    textTwlFirst:
+      "Un pequeño modal donde el diseño fue de Front end Mentor. Se desarrolló con ",
   },
   en: {
     btnCode: "view code",
     btnApp: "view app",
     lan: "EN",
-    title: "app-task-list",
-    text: "A play was development with HTML, CSS and JavaScript, a game is very know for people about of choice in paper, scicces or rock, the app too choince one.",
+    /*tech*/
+    reactText: "React and CSS",
+    sassText: "HTML, SASS and JavaScript",
+    jsText: "HTML, CSS and JavaScript",
+    styled: "React and styled-component",
+    DuoFron: "HTML ans CSS",
+    /*first project*/
+    titleOne: "my-portfolio",
+    textOneFirst: "An app that was built in ",
+    textOneSecond: " has a dark mode, and some other awesome options.",
+    /*second project*/
+    titleTwo: "app-blogr",
+    textTwoFirst:
+      "A small landing page type application that was developed with  ",
+    textTwoSecond: " the design belongs to Front end Mentor.",
+
+    /*thri project*/
+    titleThree: "rock-paper-scissors",
+    textThreeFirst: "A game that was developed in  ",
+    textThreeSecond:
+      " the game tries to choose between 3 options rock, paper or scissors.",
+    /*four project*/
+    titleFour: "easybank",
+    textFourFirst: "An app that was developed with ",
+    textFourSecond: ". The design is from Front-end mentor.",
+    /*five project*/
+    titleFive: "app-task-list",
+    textFiveFirst:
+      "A homework app that also contains a pomodoro was developed with ",
+    textFiveSecond: ". You have many more options.",
+    /*six project*/
+    titleSix: "manage-lading-page",
+    textSixFirst: "An application that was developed with  ",
+    textSixSecond:
+      ". In this project I learned to use “max-width”, the design is from Front end Mentor.",
+    /*seven project*/
+    titleSeven: "poke-search",
+    textSevenFirst:
+      "A simple application, which allows you to search for some pokemon. It was developed with  ",
+    textSevenSecond: ". The API that was consumed is from POKEAPI.",
+    /*eig project*/
+    titleEig: "app-weather",
+    textEigFirst: "An application that was developed with ",
+    textEigSecond: ". Allows you to see the time consumed by an API.",
+    /*nine project*/
+    titleNine: "advice-generator",
+    textNineFirst:
+      "A modal that gives you great advice in a random way, It consumes an additional api that was developed with  ",
+    /*ten project*/
+    titleTen: "interactive-rating",
+    textTenFirst:
+      "A modal that simulates a user's interaction when giving a product a rating. It was developed with ",
+    /*elev project*/
+    titleElev: "expense-chart",
+    textElevFirst:
+      "An application that allows you to view a graph interactively on Mobile and desktop. It was developed in ",
+    /*Twl project*/
+    titleTwl: "modal-order-sumary",
+    textTwlFirst:
+      "A small modal where the design was from Front end Mentor. It was developed with ",
   },
 };
 
