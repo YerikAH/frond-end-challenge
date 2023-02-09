8b487da2be4cd28332fbe82f04928dbe97956b86
Harvey Yerik <yerik05vh@gmail.com>
2023-02-09 13:32:07 -0500

    Dev: update project

---
+++ src/components/SectionsOne.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SectionsOne() {
+  return (
+    <div>SectionsOne</div>
+  )
+}
---
+++ src/context/dataContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, useEffect, useState } from "react";
+
+/* hook */
+import { useFetch } from "../hook/useFetch";
+
+/* interface and init state */
+import { DATA_INITIAL_STATE } from "../constant/dataInitialState";
+import { Welcome } from "../interface/data";
+import { ContextProps } from "../interface/props";
+
+const FetchContext = createContext<Welcome[]>(DATA_INITIAL_STATE);
+
+const FetchProvider = ({ children }: ContextProps) => {
+  const envUrl: string = import.meta.env.VITE_KEY_DATA_URL;
+  const [data, setData] = useState<Welcome[]>(DATA_INITIAL_STATE);
+  const { dataJson, load } = useFetch(envUrl);
+
+  useEffect(() => {
+    if (dataJson !== null) {
+      setData(dataJson);
+    }
+  }, [load]);
+
+  return <FetchContext.Provider value={data}>{children}</FetchContext.Provider>;
+};
+
+export default FetchContext;
+export { FetchProvider };
