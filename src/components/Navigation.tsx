9db2fd7fe601c4b1036605e7f7bbfa8014ef2bd8
Harvey Yerik <yerik05vh@gmail.com>
2023-02-09 13:23:13 -0500

    Dev: update navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { HeaderStyle, HeadlinePrincipal, ImageLogo, NavStyle, TextDecoration } from '../styles/navigation_style'
+import logo from "../assets/logo.svg"
 
 export default function Navigation() {
   return (
-    <div>Navigation</div>
+    <HeaderStyle>
+        <NavStyle>
+            <ImageLogo src={logo} />
+            <HeadlinePrincipal>
+                Challenge Completed
+            </HeadlinePrincipal>
+            <TextDecoration> by Harvey Yerik</TextDecoration>
+        </NavStyle>
+    </HeaderStyle>
   )
 }
--- src/styles/navigation_style.ts
+++ src/styles/navigation_style.ts
@@ -8,5 +8,21 @@ export const NavStyle = styled.nav`
     max-witdth: 1200px;
     margin: 0 auto;
     padding: 2rem 1rem;
+    display:grid;
+    place-items: center;
+    border-bottom: 1px solid #0002;
     @media (min-width:720px){}
+`
+export const ImageLogo = styled.img`
+`
+export const HeadlinePrincipal = styled.h1`
+    font: normal normal 600 1.5rem/2rem var(--font-barlow);
+    color: var(--black);
+    margin-top: 1rem;
+`
+export const TextDecoration = styled.a`
+    font: nomrla normal 400 1rem/1.5rem var(--font-barlow);
+    color: var(--blue);
+    margin-top: 0.5rem;
+    display: block;
 `
