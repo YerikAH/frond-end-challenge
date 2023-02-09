6fecfb0801b5f8db0fa5a9b0cb7d789afa664be0
Harvey Yerik <yerik05vh@gmail.com>
2023-02-09 13:01:24 -0500

    Dev: update project

---
+++ src/styles/navigation_style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const HeaderStyle = styled.header`
+    width: 100%;
+`
+export const NavStyle = styled.nav`
+    width:100%;
+    max-witdth: 1200px;
+    margin: 0 auto;
+    padding: 2rem 1rem;
+    @media (min-width:720px){}
+`
