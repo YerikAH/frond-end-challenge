5d65191acdfc02f7c51881219c3a1c5c1c0fabc1
Harvey Yerik <yerik05vh@gmail.com>
2023-02-09 13:10:33 -0500

    Dev: create interface

---
+++ src/constant/dataInitialState.ts
@@ -0,0 +1,13 @@
+import { Welcome } from "../interface/data";
+
+export const DATA_INITIAL_STATE : Welcome[]= [
+    {
+        "id":"404",
+        "name":"",
+        "image_paht":"",
+        "description": "",
+        "link_preview":"",
+        "link_code":"",
+        "link_solution":""
+    },
+]
---
+++ src/interface/data.ts
@@ -0,0 +1,9 @@
+export interface Welcome {
+    id:            string;
+    name:          string;
+    image_paht:    string;
+    description:   string;
+    link_preview:  string;
+    link_code:     string;
+    link_solution: string;
+}
