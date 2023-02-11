a669bb12b203304781ec3cfb5414e39ad291b652
Harvey Yerik <yerik05vh@gmail.com>
2023-02-10 22:00:50 -0500

    Dev: add env a

--- src/components/SectionsOne.tsx
+++ src/components/SectionsOne.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, {  useContext } from 'react'
+import FetchContext from '../context/dataContext'
+import { SectionOneStyle } from '../styles/section_one_style'
 
 export default function SectionsOne() {
+  const data = useContext(FetchContext)
   return (
-    <div>SectionsOne</div>
+    <FetchContext.Provider value={data}>
+      <SectionOneStyle></SectionOneStyle>
+      
+    </FetchContext.Provider>
   )
 }
