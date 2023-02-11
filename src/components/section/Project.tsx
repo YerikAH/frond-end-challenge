90895a2f42cfcd6defd9b7af7b5182eb1483311c
Harvey Yerik <yerik05vh@gmail.com>
2023-02-11 13:44:48 -0500

    update project

--- src/components/section/Project.tsx
+++ src/components/section/Project.tsx
@@ -1,21 +1,49 @@
-import React from 'react'
-import { ProjectProps } from '../../interface/props'
-import { ButtonProject, ButtonsBox, ButtonSpecial, ChildGrid, DescriptionProject, ImageProject, TitleProject } from '../../styles/section_one_style'
+import React from "react";
+import { ProjectProps } from "../../interface/props";
+import {
+  BoxEnd,
+  ButtonIcon,
+  ButtonProject,
+  ButtonsBox,
+  ButtonSpecial,
+  ChildGrid,
+  DescriptionProject,
+  ImageProject,
+  TitleProject,
+} from "../../styles/section_one_style";
+import logoBlue from "../../assets/icon-logo.svg";
+import linkWhite from "../../assets/icon_link_white.svg"
+import linkBlue from "../../assets/icon_link_blue.svg"
 
-export default function Project({data}: ProjectProps) {
-  console.log(data)
+export default function Project({ data }: ProjectProps) {
+  console.log(data);
   return (
     <ChildGrid>
-      <ImageProject src={data.image_paht}/>
+      <ImageProject src={data.image_paht} />
       <TitleProject>{data.name}</TitleProject>
       <DescriptionProject>{data.description}</DescriptionProject>
+      <BoxEnd>
+
+
+
       <ButtonsBox>
-        <ButtonProject href={data.link_preview}>VIEW PREVIEW</ButtonProject>
-        <ButtonProject href={data.link_code}>VIEW CODE</ButtonProject>
+        <ButtonProject href={data.link_preview}>VIEW PREVIEW
+        <ButtonIcon src={linkWhite} />
+        </ButtonProject>
+        <ButtonProject href={data.link_code} className="outline">
+          VIEW CODE
+          <ButtonIcon src={linkBlue} />
+        </ButtonProject>
       </ButtonsBox>
       <ButtonSpecial href={data.link_solution}>
+        <ButtonIcon src={logoBlue} />
         View solution in Front End Mentor
       </ButtonSpecial>
+
+
+
+
+      </BoxEnd>
     </ChildGrid>
-  )
+  );
 }
--- src/styles/section_one_style.ts
+++ src/styles/section_one_style.ts
@@ -1,78 +1,103 @@
 import styled from "styled-components";
 
 export const SectionOneStyle = styled.section`
-    width: 100%;
-    max-width: 1200px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    margin: 0 auto;
-`
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 auto;
+  padding: 0 2rem;
+  @media (min-width: 1230px) {
+    padding: 0;
+  }
+`;
 export const BoxGrid = styled.div`
-    display: grid;
-    justify-content: center;
-    margin-top: 5.625rem;
-    grid-template-columns: repeat(auto-fill, 300px);
-    width: 100%;
-    gap: 3rem;
-    @media (min-width: 1000px){
-        justify-content: space-between;
-        grid-template-columns: repeat(auto-fill, 360px);
-    }
-`
+  display: grid;
+  justify-content: center;
+  margin-top: 5.625rem;
+  grid-template-columns: repeat(auto-fill, 300px);
+  width: 100%;
+  gap: 3rem;
+  @media (min-width: 1230px) {
+    justify-content: space-between;
+    grid-template-columns: repeat(auto-fill, 360px);
+  }
+`;
 export const ChildGrid = styled.div`
-    border-radius: 1rem;
-    position: relative;
-    overflow: hidden;
-    background: rgba(255, 255, 255, 0.06);
-    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(10.5px);
-    -webkit-backdrop-filter: blur(10.5px);
-    border: 1px solid rgba(255, 255, 255, 0.24);
-
-`
+  border-radius: 1rem;
+  position: relative;
+  overflow: hidden;
+  background: rgba(255, 255, 255, 0.06);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(10.5px);
+  -webkit-backdrop-filter: blur(10.5px);
+  border: 1px solid rgba(255, 255, 255, 0.24);
+  display: flex;
+  flex-direction: column;
+`;
 export const TitleProject = styled.h3`
-    margin-top: 1rem;
-    font: normal normal 600 1.1rem/1.5rem var(--font-barlow);
-    color: var(--black);
-    text-overflow: ellipsis;
-    padding: 0 1rem;
-`
+  margin-top: 1rem;
+  font: normal normal 600 1.1rem/1.5rem var(--font-barlow);
+  color: var(--black);
+  text-overflow: ellipsis;
+  padding: 0 1rem;
+`;
 export const DescriptionProject = styled.p`
-    font: normal normal 400 0.9rem/1.5rem var(--font-barlow);
-    color: var(--black);
-    opacity: 0.7;
-    margin-top: 0.5rem;
-    padding: 0 1rem;
-`
+  font: normal normal 400 0.9rem/1.5rem var(--font-barlow);
+  color: var(--black);
+  opacity: 0.7;
+  margin-top: 0.5rem;
+  padding: 0 1rem;
+  margin-bottom: 1rem;
+`;
 
 export const ButtonsBox = styled.div`
-    padding: 0 1rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin: 0.5rem 0;
-    @media (min-width: 1000px){
-        flex-direction: row;
-        justify-content: space-between;
-    }
-`
+  padding: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  @media (min-width: 1230px) {
+    flex-direction: row;
+    justify-content: start;
+  }
+`;
 export const ImageProject = styled.img`
-    width:300px;
-    @media (min-width: 1000px){
-        width:360px;
-    }
-
-`
+  width: 300px;
+  @media (min-width: 1230px) {
+    width: 360px;
+  }
+`;
 export const ButtonProject = styled.a`
-    padding: 0.5rem 1rem;
-    border-radius: 15rem;
-    font-weight: 500;
-    text-transform: uppercase;
-    display: block;
-    font-style: italic;
-`
-export const ButtonSpecial= styled.a``
-export const ButtonIcon = styled.img``
+  padding: 0.5rem 1rem;
+  font-weight: 500;
+  text-transform: uppercase;
+  display: flex;
+  font-style: italic;
+  border-radius: 15rem;
+  background-color: ${(props) =>
+    props.className === "outline" ? "var(--white)" : "var(--blue)"};
+  border: ${(props) =>
+    props.className === "outline" ? "1px solid var(--blue)" : "none"};
+  color: ${(props) =>
+    props.className === "outline" ? "var(--blue)" : "var(--white)"};
+  font-size: 0.9rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+`;
+export const ButtonSpecial = styled.a`
+  display: flex;
+  margin: 0.5rem 1rem;
+  padding: 0.5rem 1rem;
+  background: var(--white-pale);
+  color: var(--black);
+  font: normal normal 500 0.9rem/1.2rem var(--font-barlow);
+  border-radius: 0.3rem;
+  gap: 1rem;
+`;
+export const ButtonIcon = styled.img``;
+export const BoxEnd = styled.div``
