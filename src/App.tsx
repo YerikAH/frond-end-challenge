e0667047f1b3593300993526680ea8d7d9e3c0c5
Harvey Yerik <yerik05vh@gmail.com>
2023-02-10 21:34:49 -0500

    Dev: add env v

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Navigation from "./components/Navigation"
+import { FetchProvider } from "./context/dataContext"
 
 function App() {
   return (
-    <>
+    <FetchProvider>
       <Navigation/>
-    </>
+    </FetchProvider>
   )
 }
 
