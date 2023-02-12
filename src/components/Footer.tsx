c1367cca35facd9b9e9f5acd35a68d8a0e9430ac
Harvey Yerik <yerik05vh@gmail.com>
2023-02-12 12:45:19 -0500

    Dev: update project

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,40 @@
+import { FooterStyles, LinkFooter, ListFooter, LogoText, PointFooter } from "../styles/footer_style";
+import { BoxGrid } from "../styles/section_one_style";
+import GitHubIcon from "./icons/GitHubIcon";
+import LinkedinIcon from "./icons/LinkedinIcon";
+
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <FooterStyles>
+      <BoxGrid>
+        <LogoText> Challenges completed </LogoText>
+        <ListFooter>
+
+          <PointFooter>
+            <LinkFooter href="">
+              <GitHubIcon/>
+            </LinkFooter>
+          </PointFooter>
+
+          <PointFooter>
+            <LinkFooter href="">
+              <LinkedinIcon/>
+            </LinkFooter>
+          </PointFooter>
+
+          <PointFooter>
+            <LinkFooter href="">
+              <GitHubIcon/>
+            </LinkFooter>
+          </PointFooter>
+
+
+        </ListFooter>
+
+
+      </BoxGrid>
+
+
+    </FooterStyles>
   )
 }
